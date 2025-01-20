b4cc4ca0b7c4d1088f078481140a1a01785cf6fc
Mohamed El Sayed <alsaidm508@gmail.com>
2025-01-20 11:13:26 +0200

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPC_
+{
+    public class Rectangle : IRectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Area 
+        {  
+            get {return  Width * Height; }  
+        }
+
+        public void DisplayShapeInfo()
+        {
+            
+            Console.WriteLine($"The Area Of The Rectangle = {Area}");
+        }
+    }
+}
